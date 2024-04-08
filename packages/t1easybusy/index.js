c04ccd67c267cdb283d41aab84d597c162566252
amitwa1 <153432188+amitwa1@users.noreply.github.com>
2024-04-08 17:10:24 +0300

    start

---
+++ packages/t1easybusy/index.js
@@ -0,0 +1,10 @@
+exports.log = () => {
+    console.log("a");
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+}
---
+++ packages/t2easybusy/index.js
@@ -0,0 +1,9 @@
+const a = require('a');
+a.log();
+console.log("b")
+console.log("b")
+console.log("b")
+console.log("b")
+console.log("b")
+console.log("b")
+console.log("b")
