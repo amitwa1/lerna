beccc3cb4822c2ec0df301686d1409b0264c5271
amitwa1 <153432188+amitwa1@users.noreply.github.com>
2024-04-09 08:52:53 +0300

    start

---
+++ packages/t3easybusy/index.js
@@ -0,0 +1,11 @@
+exports.log = () => {
+    console.log("a");
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+}
