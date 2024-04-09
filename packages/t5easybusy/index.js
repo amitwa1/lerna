54ccf4ed6e44e1e16e2d4e0861db121b63fe8dde
amitwa1 <153432188+amitwa1@users.noreply.github.com>
2024-04-09 09:11:25 +0300

    P1 (#1)
    * start
    
    * start
    
    * Publish
    
     - t5easybusy@1.0.1-alpha.0

---
+++ packages/t5easybusy/index.js
@@ -0,0 +1,14 @@
+exports.log = () => {
+    console.log("a");
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+    console.log("22")
+}
